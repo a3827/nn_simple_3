58bf5a27058ca8669d1b59799e92b667f8c17078
arek <areklib@poczta.onet.pl>
2023-04-02 10:10:18 +0200

    Init

---
+++ nn_simple_3.py
@@ -0,0 +1,10 @@
+#How to Create a Simple Neural Network Model in Python
+#Building a Simple Neural Network with Scikit-Learn to Predict Well Log Measurements
+#
+#This is more advanced example, with real data.
+#
+#https://towardsdatascience.com/how-to-create-a-simple-neural-network-model-in-python-70697967738f
+#
+#Andy Mac Donald
+#Petrophysicist and Data Scientist with a passion for data analytics, machine learning, and AI. https://andymcdonaldgeo.medium.com/membership
+#
